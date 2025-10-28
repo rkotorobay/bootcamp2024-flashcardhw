cd8971665c7b87caa5daadbbae62a1a38c0172b3
Roman Kotorobay <romankotorobay.rk@gmail.com>
2025-10-28 14:42:47 -0400

    implemented the js for this project

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ const flashcards = [
     { term: "JavaScript", definition: "Programming language of the web" }
 ];
 
+const card = document.getElementById("flashcard");
+card.onclick = flipCard;
+
+const nextButton = document.getElementById("next-btn");
+nextButton.onclick = next;
+
+const prevButton = document.getElementById("prev-btn");
+prevButton.onclick = previous;
+
+const addCardButton = document.getElementById("add-card-btn");
+addCardButton.onclick = addCard;
 // You can use flashcards.length to get the length of the array
 
 // These two variables will come in handy
@@ -12,7 +23,50 @@ let showingTerm = true;
 
 // Start with this function to simply display the card
 function displayCard() {
+    const currCard = flashcards[currentIndex];
+    if(showingTerm){
+        document.getElementById("card-content").innerHTML = currCard.term;
+    }else {
+        document.getElementById("card-content").innerHTML = currCard.definition;
+    }
+}
 
+function flipCard() {
+    if(showingTerm) {
+        showingTerm = false;
+    } else {
+        showingTerm = true;
+    }
+    displayCard();
+}
+
+function next() {
+    if(currentIndex == flashcards.length - 1) {
+        currentIndex = 0;
+    } else {
+        currentIndex++;
+    }
+    displayCard();
+}
+
+function previous() {
+    if(currentIndex == 0) {
+        currentIndex = flashcards.length - 1;
+    } else {
+        currentIndex--;
+    }
+    displayCard();
+}
+
+function addCard() {
+    const newTerm = document.getElementById("new-term").value;
+    const newDef = document.getElementById("new-definition").value;
+
+    if(newTerm === "" || newDef === ""){
+        alert("Missing input for term OR definition");
+        return;
+    }
+    flashcards.push({term: newTerm, definition: newDef});
 }
 
 // The rest of the code you will write is apart of event listeners
